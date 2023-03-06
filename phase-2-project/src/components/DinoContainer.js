6090d3391ec9866593b02ee071e1f72e07db333f
Mark Coats <alexco1234@gmail.com>
2023-03-06 12:00:18 -0600

    ye

--- phase-2-project/src/components/DinoContainer.js
+++ phase-2-project/src/components/DinoContainer.js
@@ -1,7 +1,20 @@
 import React from 'react'
 import DinoCard from './DinoCard'
+import { useState, useEffect } from 'react';
+import { Route, useMatch, useParams } from 'react-router-dom';
 
-function DinoContainer () {
+function DinoContainer ({array}) {
+
+    // let match = useMatch();
+    // console.log(match.url);
+    let link = array.map(dino => {
+        return <DinoCard key={dino.name} data={dino} dinos={array}/>
+    })
+    return (
+        <div className='dinoCardContainer'>
+            {link}
+        </div>
+    )
 
 }
 
--- phase-2-project/src/components/Dinos.js
+++ phase-2-project/src/components/Dinos.js
@@ -1,7 +1,30 @@
 import React from 'react'
 import DinoContainer from './DinoContainer'
+import NavBar from './NavBar';
+import { useState, useEffect, useParams } from 'react';
 
 function Dinos () {
+    // let params = useParams();
+    // console.log(params);
+    const [dinoArray, setDinoArray] = useState([]);
+    const [searchQuery, setSearchQuery] = useState('');
+    const [searchedArr, setSearchedArr] = useState([]);
+
+    let port = 'http://localhost:3001/dinos';
+
+    useEffect(() => {
+        fetch(port)
+            .then(res => res.json())
+            .then(returnedArray => setDinoArray(returnedArray))
+    },[])
+
+    return (
+        <>
+            <NavBar />
+            Dino Page!
+            <DinoContainer array={dinoArray}/>
+        </>
+    )
 
 }
 
