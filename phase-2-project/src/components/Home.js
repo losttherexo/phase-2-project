0b9fae8ae5ab1c1a4317ce9f2948f98c503b7977
Robert Hitchcock <robert.e.hitchcock@gmail.com>
2023-03-06 15:50:08 -0500

    form options

--- phase-2-project/src/components/Home.js
+++ phase-2-project/src/components/Home.js
@@ -6,6 +6,28 @@ function Home () {
         <>
             <NavBar />
             This is Home!
+            <form id="dinoForm">
+                <h2>Add a New Dinosaur</h2>
+                <input type="text" placeholder="Dinosaur Name" />
+                <input type="text" placeholder="Dinosaur Image" />
+                <input type="text" placeholder="Dinosaur Diet" />
+                <input type="text" placeholder="Dinosaur Habitat" />
+                <input type="text" placeholder="Dinosaur Location" />
+                <input type="text" placeholder="Dinosaur Time Period" />
+                <input type="number" placeholder="Dinosaur Weight" />
+                <input type="number" placeholder="Dinosaur Height" />
+                <input type="text" placeholder="Dinosaur Fun Fact" />
+                <button type="submit">Submit Dinosaur</button>
+            </form>
+            <br></br>
+            <form id="dinoMovie">
+                <h2>Add a New Movie</h2>
+                <input type="text" placeholder="Movie Title" />
+                <input type="text" placeholder="Movie Image" />
+                <input type="text" placeholder="Movie Director" />
+                <input type="text" placeholder="Movie Summary" />
+                <button type="submit">Submit Movie</button>
+            </form>
         </>
     )
 
