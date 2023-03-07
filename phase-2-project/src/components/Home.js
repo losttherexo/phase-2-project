76321cd852acfbde85e2abdc526e1c30fdcbce5e
André Vargas Roo <a.vargas252@icloud.com>
2023-03-07 15:07:30 -0500

    working on application-wide background and other basic css

--- phase-2-project/src/components/Home.js
+++ phase-2-project/src/components/Home.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import NavBar from './NavBar';
 
 function Home () {
     return (
     <div className='home'>
         <h1>Dino-More</h1>
         <h2>Dinosaur Database of Movies and Information of Multiple Species </h2>
-        <br></br>
         <img className="homeDinoPic" src="https://wallpaper.dog/large/11037831.jpg"/>
     </div>
     )
