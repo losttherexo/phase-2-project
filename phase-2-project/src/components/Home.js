9dad484d8a076f30e7f666d4c777c45ff8d1e1b1
André Vargas Roo <a.vargas252@icloud.com>
2023-03-07 11:01:08 -0500

    home screen centered, animation added to movie slides

--- phase-2-project/src/components/Home.js
+++ phase-2-project/src/components/Home.js
@@ -3,17 +3,13 @@ import NavBar from './NavBar';
 
 function Home () {
     return (
-        <>
-      <h1>Dino-More</h1>
-      <h2>Dinosaur Database of Movies and Information of Multiple Species </h2>
-       <br></br>
-    <img className="homeDinoPic" src="https://wallpaper.dog/large/11037831.jpg"/>
-
-            <br></br>
-            
-        </>
+    <div className='home'>
+        <h1>Dino-More</h1>
+        <h2>Dinosaur Database of Movies and Information of Multiple Species </h2>
+        <br></br>
+        <img className="homeDinoPic" src="https://wallpaper.dog/large/11037831.jpg"/>
+    </div>
     )
-
 }
 
 export default Home;
