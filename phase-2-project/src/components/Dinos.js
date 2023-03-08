4ee579df057b41ee98bc9a49c38d259ad2232ce8
Robert Hitchcock <robert.e.hitchcock@gmail.com>
2023-03-08 10:03:41 -0500

    fixed form css

--- phase-2-project/src/components/Dinos.js
+++ phase-2-project/src/components/Dinos.js
@@ -88,7 +88,11 @@ function Dinos () {
 
             </div>
             <button onClick={() => sethideAddForm(!hideAddForm)} id='addDBtn'>Add Dinosaurs</button>
-            <form className= "form-box" hidden={hideAddForm} id="dinoForm" onSubmit={handleSubmit}>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <form className= "form-box1" hidden={hideAddForm} id="dinoForm" onSubmit={handleSubmit}>
                 <h2>Add a New Dinosaur</h2>
                 <input onChange={handleChange} value={dForm.name}name="name"type="text" placeholder="Dinosaur Name" />
                 <input onChange={handleChange} value={dForm.image}name="image"type="text" placeholder="Dinosaur Image" />
@@ -99,8 +103,9 @@ function Dinos () {
                 <input onChange={handleChange} value={dForm.weight}name="weight"type="number" placeholder="Dinosaur Weight" />
                 <input onChange={handleChange} value={dForm.height}name="height"type="number" placeholder="Dinosaur Height" />
                 <input onChange={handleChange} value={dForm.fact} name="fact" type="text" placeholder="Dinosaur Fun Fact" />
-                <button className="submitB" type="submit">Submit Dinosaur</button>
+                <button className="submitB1" type="submit">Submit Dinosaur</button>
             </form>
+            <br></br>
         </>
     )
         /**
