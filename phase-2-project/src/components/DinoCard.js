53c38a16fe237a7e3892d1d1c0b1e51ecc3e8424
André Vargas Roo <a.vargas252@icloud.com>
2023-03-08 16:52:02 -0500

    cleanup time

--- phase-2-project/src/components/DinoCard.js
+++ phase-2-project/src/components/DinoCard.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import { Link, useParams, useMatch } from 'react-router-dom';
 
 function DinoCard ({data, displayDino}) {
     //<Link to={`/dinos/${dino.name}`}>{dino.name}</Link>//
     //const match = useMatch();
-    const {name, image, diet, habitat, location, funFact} = data;
+    const {name, image} = data;
 
     const handleClick = () => {
         console.log('helloWOrld', name)
@@ -22,8 +21,6 @@ function DinoCard ({data, displayDino}) {
             <img className='DinoImage' src={image} alt={name}></img>
         </div>
     )
-
-
 }
 
 export default DinoCard;
--- phase-2-project/src/components/NavBar.js
+++ phase-2-project/src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route, Link, NavLink } from 'react-router-dom';
+import {NavLink } from 'react-router-dom';
 
 function NavBar () {
     return (
