c0f426b5dd823706ea8bfbe00b654a211cbf9a45
Andre Vargas Roo <a.vargas252@icloud.com>
2023-03-10 11:01:18 -0500

    dang search design

--- phase-2-project/src/components/Search.js
+++ phase-2-project/src/components/Search.js
@@ -16,7 +16,7 @@ function Search ({searcher}) {
 
     return (
         <div className='search py-4' >
-            <form className="flex justify-center py-3 bg-[#5F5F5F] mx-[420px] rounded-lg" onSubmit={handleSubmit}>
+            <form className="flex justify-center py-3 bg-[#5F5F5F] mx-12 rounded-lg" onSubmit={handleSubmit}>
                 <input className='px-2' onChange={e => handleChange(e.target.value)} value={searchQ} name="searchQ" type="text" placeholder="Search..." />
                 <span className='px-2' ><button type='submit'>🔍</button></span>
             </form>
